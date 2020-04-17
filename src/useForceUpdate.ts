7014f188b8e76dd7e08c3cb1dfb63beaaa8f2a6e
Josh Pensky <joshuapensky@gmail.com>
2020-04-17 16:31:44 -0400

    adds last bit of JSDoc to useSet, useMap, and useForceUpdate

--- src/useForceUpdate.ts
+++ src/useForceUpdate.ts
@@ -1,6 +1,23 @@
 import { useDebugValue } from 'react';
 import useState from './useState';
 
+/**
+ * Custom React utility hook that provides a way to force the a component to update. It's
+ * recommended to _only be used_ when the DOM is dependent on a ref value.
+ *
+ * @example
+ * ```js
+ * const update = useForceUpdate();
+ * const ref = useRef(1);
+ *
+ * const increase = () => {
+ *   ref.current += 1;
+ *   update();
+ * };
+ * ```
+ *
+ * @returns an update function that, when called, will force the component to update
+ */
 const useForceUpdate = () => {
   const [tick, setTick] = useState(-1);
   const update = () => setTick(tick => tick + 1);
